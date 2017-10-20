2c786a348c354a21cc00ef99137b6a06d2185e0d
Markus Malkusch <markus@malkusch.de>
2017-10-20 14:49:49 +0200

    Trim events which exceed AWS' limit of 262144 byte
    There's an undocumented limit of 262144 bytes per single log event.
    Amazon gives in its documentation about the batch size a hint how to
    determine the size of a single log event (message size in UTF-8 + 26).
    
    See also: http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
    See also: #6

--- src/main/java/ca/pjer/logback/Worker.java
+++ src/main/java/ca/pjer/logback/Worker.java
@@ -1,6 +1,9 @@
 package ca.pjer.logback;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.nio.charset.Charset;
+
 import com.amazonaws.services.logs.model.InputLogEvent;
 
 abstract class Worker {
@@ -23,8 +26,40 @@ abstract class Worker {
 
     public abstract void append(ILoggingEvent event);
 
+    // See https://github.com/pierredavidbelanger/logback-awslogs-appender/issues/6
+    private static final int MAX_EVENT_SIZE = 262144;
+
     InputLogEvent asInputLogEvent(ILoggingEvent event) {
-        return new InputLogEvent().withTimestamp(event.getTimeStamp())
+        InputLogEvent inputLogEvent = new InputLogEvent().withTimestamp(event.getTimeStamp())
                 .withMessage(awsLogsAppender.getLayout().doLayout(event));
+
+        if (eventSize(inputLogEvent) > MAX_EVENT_SIZE) {
+            awsLogsAppender
+                    .addWarn(String.format("Log message exceeded Cloudwatch Log's limit of %d bytes", MAX_EVENT_SIZE));
+            trimMessage(inputLogEvent, MAX_EVENT_SIZE);
+        }
+
+        return inputLogEvent;
+    }
+
+    // See http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
+    private static final int EVENT_SIZE_PADDING = 26;
+    private static final Charset EVENT_SIZE_CHARSET = Charset.forName("UTF-8");
+
+    static final int eventSize(InputLogEvent event) {
+        return event.getMessage().getBytes(EVENT_SIZE_CHARSET).length + EVENT_SIZE_PADDING;
+    }
+
+    private static final String ELLIPSIS = "...";
+
+    private static final void trimMessage(InputLogEvent event, int eventSize) {
+        int trimmedMessageSize = eventSize - EVENT_SIZE_PADDING - ELLIPSIS.getBytes(EVENT_SIZE_CHARSET).length;
+        byte[] message = event.getMessage().getBytes(EVENT_SIZE_CHARSET);
+
+        String unsafeTrimmed = new String(message, 0, trimmedMessageSize + 1, EVENT_SIZE_CHARSET);
+        // The last character might be a chopped UTF-8 character
+        String trimmed = unsafeTrimmed.substring(0, unsafeTrimmed.length() - 1);
+
+        event.setMessage(trimmed + ELLIPSIS);
     }
 }
---
+++ src/test/java/ca/pjer/logback/WorkerTest.java
@@ -0,0 +1,92 @@
+package ca.pjer.logback;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Before;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import com.amazonaws.services.logs.model.InputLogEvent;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.layout.EchoLayout;
+
+@RunWith(Theories.class)
+public class WorkerTest {
+
+    private static final Layout<ILoggingEvent> LAYOUT = new EchoLayout<ILoggingEvent>();
+    private static final int LAYOUT_OFFSET = LAYOUT.doLayout(asEvent("")).length();
+    private static final int MESSAGE_SIZE_LIMIT = 262144 - 26 - LAYOUT_OFFSET;
+    private Worker worker;
+
+    @Before
+    public void setWorker() {
+        AwsLogsAppender awsLogsAppender = new AwsLogsAppender();
+        awsLogsAppender.setLayout(LAYOUT);
+        worker = new SyncWorker(awsLogsAppender);
+    }
+
+    @DataPoints("UNTRIMMED")
+    public static final String[] UNTRIMMED = {
+            repeat("x", MESSAGE_SIZE_LIMIT),
+            repeat("x", MESSAGE_SIZE_LIMIT - 2) + "ö" };
+
+    @Theory
+    public void eventShouldNotBeTrimmed(@FromDataPoints("UNTRIMMED") String message) {
+        InputLogEvent event = worker.asInputLogEvent(asEvent(message));
+        assertFalse(event.getMessage().endsWith("..."));
+    }
+    
+    @DataPoints("TRIMMED")
+    public static final String[] TRIMMED = {
+            repeat("x", MESSAGE_SIZE_LIMIT + 1),
+            repeat("x", MESSAGE_SIZE_LIMIT - 1) + "ö",
+            repeat("x", MESSAGE_SIZE_LIMIT - 2) + "öö",
+            repeat("x", MESSAGE_SIZE_LIMIT - 3) + "öö",
+            repeat("x", MESSAGE_SIZE_LIMIT) + "ö" };
+
+    @Theory
+    public void eventShouldBeTrimmed(@FromDataPoints("TRIMMED") String message) {
+        InputLogEvent event = worker.asInputLogEvent(asEvent(message));
+        assertTrue(event.getMessage().endsWith("..."));
+    }
+    
+    @DataPoints("TRIMMED_MB")
+    public static final String[] TRIMMED_MB = {
+            repeat("x", MESSAGE_SIZE_LIMIT - 9) + "öööööööööö",
+            repeat("x", MESSAGE_SIZE_LIMIT - 8) + "öööööööööö",
+            repeat("x", MESSAGE_SIZE_LIMIT - 7) + "öööööööööö", };
+
+    @Theory
+    public void trimmingShouldNotChopMultibyteCharacter(@FromDataPoints("TRIMMED_MB") String message) {
+        InputLogEvent event = worker.asInputLogEvent(asEvent(message));
+        assertTrue(event.getMessage().endsWith("ö..."));
+    }
+
+    @Theory
+    public void eventShouldNeverExceed262144Bytes(String message) throws UnsupportedEncodingException {
+        InputLogEvent event = worker.asInputLogEvent(asEvent(message));
+        int eventSize = event.getMessage().getBytes("UTF-8").length + 26;
+        assertTrue(eventSize <= 262144);
+    }
+
+    private static ILoggingEvent asEvent(String message) {
+        LoggerContext loggerContext = new LoggerContext();
+        return new LoggingEvent(null, loggerContext.getLogger(WorkerTest.class), Level.INFO, message, null, null);
+    }
+
+    private static String repeat(String value, int count) {
+        return new String(new char[count]).replace("\0", value);
+    }
+
+}
